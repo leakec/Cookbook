27d877fa3672bd824e90df7f895b49203046d3bd
leakec <leakec57@gmail.com>
2023-02-24 10:11:24 -0800

    Formatted with Black.

--- scripts/website_email_send.py
+++ scripts/website_email_send.py
@@ -4,22 +4,25 @@ import requests
 MY_EMAIL = "example@example.com"
 MY_PASS = "dummpyPass"
 
+
 def get_ngrok_url():
     url = "http://localhost:4040/api/tunnels/"
     res = requests.get(url)
     res_unicode = res.content.decode("utf-8")
     res_json = json.loads(res_unicode)
     for i in res_json["tunnels"]:
-        if i['name'] == 'command_line':
-            return i['public_url']
+        if i["name"] == "command_line":
+            return i["public_url"]
             break
 
+
 import smtplib
-#SERVER = "localhost"
 
-FROM = MY_EMAIL 
+# SERVER = "localhost"
 
-TO = [MY_EMAIL] # must be a list
+FROM = MY_EMAIL
+
+TO = [MY_EMAIL]  # must be a list
 
 SUBJECT = "Cook Book URL"
 
@@ -32,13 +35,18 @@ To: %s
 Subject: %s
 
 %s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+""" % (
+    FROM,
+    ", ".join(TO),
+    SUBJECT,
+    TEXT,
+)
 
 print(message)
 
 # Send the email
-#server = smtplib.SMTP('smtp.gmail.com', 587)
-#server.starttls()
-#server.login(MY_EMAIL,MY_PASS)
-#server.sendmail(FROM, TO, message)
-#server.quit()
+# server = smtplib.SMTP('smtp.gmail.com', 587)
+# server.starttls()
+# server.login(MY_EMAIL,MY_PASS)
+# server.sendmail(FROM, TO, message)
+# server.quit()
