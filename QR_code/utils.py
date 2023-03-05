e53ab9cc48f5d2c72f38d375b7ada41a00b0179d
leakec <leakec57@gmail.com>
2023-03-05 08:46:09 -0800

    Using sihm for remap function now.

--- QR_code/utils.py
+++ QR_code/utils.py
@@ -6,25 +6,12 @@ from pathlib import Path
 Color = Tuple[float, float, float, float]
 
 
-def lerp(from_val: Any, to_val: Any, t: Any) -> Any:
-    """
-    Linearly interpolate.
-    """
-    return (1.0 - t) * from_val + t * to_val
-
-
-def inv_lerp(from_val: Any, to_val: Any, t: Any) -> Any:
-    """
-    Inverse lerp function.
-    """
-    return (t - from_val) / (to_val - from_val)
-
-
 def remap(c1: Color, c2: Color, c3: Color, c4: Color, image: Path, new_image: Path):
     """
     Remap from c1 and c2 to c3 and c4.
     """
     from PIL import Image
+    from sihm.utils import remap
 
     c1 = np.array(c1).reshape((1, 1, 4))
     c2 = np.array(c2).reshape((1, 1, 4))
@@ -34,8 +21,7 @@ def remap(c1: Color, c2: Color, c3: Color, c4: Color, image: Path, new_image: Pa
     with Image.open(image) as f:
         pix_vals = np.array(f)
 
-    t = inv_lerp(c1, c2, pix_vals / 255.0)
-    new_pix_vals = lerp(c3, c4, t) * 255.0
+    new_pix_vals = remap(c1, c2, c3, c4, pix_vals / 255.0) * 255.0
     new_pix_vals = np.nan_to_num(new_pix_vals, nan=255.0)
     new_pix_vals = np.array(new_pix_vals, dtype=np.uint8)
 
